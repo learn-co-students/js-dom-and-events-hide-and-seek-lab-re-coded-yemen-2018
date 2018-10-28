df250b655d1b1367a446376d6a71c2f30a8ab1d7
Farida <faridah.absi@gmail.com>
2018-10-28 10:29:47 +0000

    Done.

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ function getFirstSelector(selector){
   return document.querySelector(selector);
  }
 function nestedTarget(){
- 
+
   let target =document.querySelector("#nested").querySelector(".target");
    return target;
 }
 function increaseRankBy(n) {
     let childrens = document.querySelectorAll(".ranked-list li");
-    
+
      for (let i = 0; i < childrens.length; i++) {
-        let result = parseInt(childrens[i].innerHTML);
-        childrens.innerHTML = result + n;
+          const cn =  childrens[i];
+        let result = parseInt(cn.innerHTML);
+        cn .innerHTML = result + n ;
      }
 }
 
 function deepestChild(){
   let child =document.querySelector("#grand-node div div div div");
   return child;
-  
+
 }
